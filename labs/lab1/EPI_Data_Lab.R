966289ee53a131fe7cbc745d92d2c868c6464a43
RussellABH <75578919+RussellABH@users.noreply.github.com>
2022-09-16 08:50:58 -0400

    lab 1 done I believe

--- labs/lab1/EPI_Data_Lab.R
+++ labs/lab1/EPI_Data_Lab.R
@@ -3,11 +3,32 @@ EPI_data <- read.csv("2010EPI_data.csv")
 names(EPI_data) <- EPI_data[1,]
 EPI_data <- EPI_data[-1,]
 View(EPI_data)
-# Need to convert values to numerics
 
+# Gather and transform EPI. I don't like attach()
+#  For other variables uncomment that code
 
-EPI <- EPI_data$EPI # I don't like attaching so I'm going to do this
+# data <- na.omit(as.numeric(EPI_data$EPI))
+# data <- na.omit(as.numeric(EPI_data$DALY))
+data <- na.omit(as.numeric(EPI_data$WATER_H))
+
 
 # view some data
-colnames(EPI_data)
-summary(EPI)
+summary(data)
+
+# Stem + Histogram + Box plot
+fivenum(data, na.rm=T)
+stem(data)
+hist(data)
+#hist(data,seq(30., 95., 1.0), prob=T)
+hist(data, freq=F); lines(density(data, bw="SJ")); rug(data)
+boxplot(data)
+
+# More complicated graphs
+plot(ecdf(data), do.points=F,verticals=T)
+qqnorm(data); qqline(data)
+
+# Compare all three variables
+allData = data.frame(EPI = na.omit(as.numeric(EPI_data$EPI)), 
+                     DALY = na.omit(as.numeric(EPI_data$DALY)), 
+                     WATER_H = na.omit(as.numeric(EPI_data$WATER_H)))
+boxplot(allData)
