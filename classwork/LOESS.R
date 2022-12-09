d54a8fd67853ef7b976cbc7dd806a46a270b4f46
Russell Harter <rabh.2202@gmail.com>
2022-12-09 10:08:59 -0500

    random class stuff

---
+++ classwork/LOESS.R
@@ -0,0 +1,33 @@
+# LOESS Exaplme from:
+# http://r-statistics.co/Loess-Regression-With-R.html
+data(economics, package="ggplot2") # load data
+economics$index <- 1:nrow(economics) # create index variable
+economics <- economics[1:80, ] # retail 80rows for better graphical understanding
+loessMod10 <- loess(uempmed ~ index, data=economics, span=0.10) # 10% smoothing span
+loessMod25 <- loess(uempmed ~ index, data=economics, span=0.25) # 25% smoothing span
+loessMod50 <- loess(uempmed ~ index, data=economics, span=0.50) # 50% smoothing span
+loessMod80 <- loess(uempmed ~ index, data=economics, span=0.80) # 80% smoothing span
+loessMod90 <- loess(uempmed ~ index, data=economics, span=0.90) # 90% smoothing span
+loessMod97 <- loess(uempmed ~ index, data=economics, span=0.97) # 97% smoothing span
+
+# Predict Loess
+smoothed10 <- predict(loessMod10)
+smoothed25 <- predict(loessMod25)
+smoothed50 <- predict(loessMod50)
+smoothed80 <- predict(loessMod80)
+smoothed90 <- predict(loessMod90)
+smoothed97 <- predict(loessMod97)
+
+
+# From above plot, you would notice that as the span increases, the smoothing of the curve also increases.
+# Code for Plot
+# Plot it
+plot(economics$uempmed, x=economics$date, type="l", main="Loess Smoothing and Prediction",
+     xlab="Date", ylab="Unemployment (Median)")
+lines(smoothed10, x=economics$date, col="red")
+lines(smoothed25, x=economics$date, col="green")
+lines(smoothed50, x=economics$date, col="blue") 
+lines(smoothed80, x=economics$date, col="orange") 
+lines(smoothed90, x=economics$date, col="pink") 
+lines(smoothed97, x=economics$date, col="purple") 
+
